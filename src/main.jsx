7b3c8a7f980afa72fc742c54969048226e983ead
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-29 12:40:44 +0600

    Router setuo and folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AuthProvicer from "./assets/providers/AuthProvider/AuthProvicer.jsx";
+import Root from "./assets/layout/Root/Root.jsx";
+import Dashboard from "./assets/pages/Dashboard.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <Dashboard />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AuthProvicer>
+            <RouterProvider router={router}></RouterProvider>
+        </AuthProvicer>
+    </React.StrictMode>
+);
