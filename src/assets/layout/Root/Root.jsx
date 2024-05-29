54c3e0db0f4844fba39a2f853d9b09ee899317da
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-29 14:14:39 +0600

    navbar create

--- src/assets/layout/Root/Root.jsx
+++ src/assets/layout/Root/Root.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../../components/NavBar";
+import SidebarNav from "../../components/SidebarNav";
 
 const Root = () => {
     return (
         <>
-            <Outlet/>
+            <NavBar />
+            {/* <SidebarNav/> */}
         </>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,14 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
-  ]
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  }
 }
